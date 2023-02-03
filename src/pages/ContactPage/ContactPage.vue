6856429f045264f30df988c08eb0ab9d76668773
Lev Kaplunov <limon4ik59rus@mail.ru>
2023-02-03 12:18:07 +0500

    Add target blank

--- src/pages/ContactPage/ContactPage.vue
+++ src/pages/ContactPage/ContactPage.vue
@@ -1,9 +1,33 @@
 <template>
   <div class="contact">
     <ul>
-      <li>Email: <a href="mailto:lev.kaplunov@yandex.ru">lev.kaplunov@yandex.ru</a></li>
-      <li>Telegram: <a href="https://t.me/lev_kaplunov">Lev Kaplunov</a></li>
-      <li>GitHub: <a href="https://github.com/lionlimon">lionlimon</a></li>
+      <li>
+        Email:
+        <a
+          target="_blank"
+          href="mailto:lev.kaplunov@yandex.ru"
+        >
+          lev.kaplunov@yandex.ru
+        </a>
+      </li>
+      <li>
+        Telegram:
+        <a
+          target="_blank"
+          href="https://t.me/lev_kaplunov"
+        >
+          Lev Kaplunov
+        </a>
+      </li>
+      <li>
+        GitHub:
+        <a
+          target="_blank"
+          href="https://github.com/lionlimon"
+        >
+          lionlimon
+        </a>
+      </li>
     </ul>
   </div>
 </template>
